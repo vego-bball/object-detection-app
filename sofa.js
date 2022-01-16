1d5852183e7fdbab43519ca5dc314a64dffaadc8
Veer <80690781+vego-bball@users.noreply.github.com>
2022-01-16 13:58:55 +0700

    Add files via upload

---
+++ sofa.js
@@ -0,0 +1,35 @@
+status = "";
+
+function preload()
+{
+    img = loadImage("Sofa.jpg");
+}
+
+function setup()
+{
+    canvas = createCanvas(640 ,420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results)
+{
+    if (error)
+    {
+        console.log(error);
+    }
+    console.log(results);
+}
+
+function back()
+{
+    window.location("index.html");
+}
